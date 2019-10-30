086b8d265fe1bde2c78996d8f49d4624d2d74e97
vlaksi <daksrapsody@gmail.com>
2019-10-30 23:17:26 +0100

    v3 priprema

---
+++ vezbe[zadaci]/v3/gs.m
@@ -0,0 +1,15 @@
+function [x, it] = gs(A, b, x0, itMax, errMax)
+    [rows, cols] = size(A);
+    x = x0;
+    for it = 1:itMax
+        for row = 1:rows
+            x(row) = 1/A(row,row)*(b(row) - A(row,1:row - 1)*x(1:row - 1) - A(row,row + 1:end)*x0(row + 1:end));
+        end
+        if abs(x - x0) < errMax
+            return
+        end
+        x0 = x;
+    end
+end
+        
+        
